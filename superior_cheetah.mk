472fa7f56a22308edac76304d6e54e5f51efacff
kenway214 <kenway214@outlook.com>
2023-12-21 16:43:47 +0530

    panther: superior-ify
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- superior_cheetah.mk
+++ superior_cheetah.mk
@@ -4,9 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common Superior stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_cheetah.mk)
@@ -14,10 +15,24 @@ $(call inherit-product, device/google/gs201/lineage_common.mk)
 
 include device/google/pantah/cheetah/device-lineage.mk
 
+# PixelParts
+include packages/apps/PixelParts/device.mk
+
+# Call the BCR setup
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7 Pro
-PRODUCT_NAME := lineage_cheetah
+PRODUCT_NAME := superior_cheetah
+
+# superiorOS additions
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+BUILD_WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3120
--- superior_panther.mk
+++ superior_panther.mk
@@ -4,9 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common Superior stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
@@ -14,10 +14,24 @@ $(call inherit-product, device/google/gs201/lineage_common.mk)
 
 include device/google/pantah/panther/device-lineage.mk
 
+# PixelParts
+include packages/apps/PixelParts/device.mk
+
+# Call the BCR setup
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7
-PRODUCT_NAME := lineage_panther
+PRODUCT_NAME := superior_panther
+
+# superiorOS additions
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+BUILD_WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
