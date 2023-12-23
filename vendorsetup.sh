a23b7f586b1ff5b21f8f06e6488d577cf955233a
kenway214 <kenway214@outlook.com>
2023-12-24 01:47:22 +0530

    cheetah: pixel dependency

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -23,4 +23,18 @@ git clone https://github.com/kenway214/packages_apps_PixelParts.git -b lineage-2
 echo 'Cloning bcr'
 git clone --depth=1 https://github.com/kenway214/vendor_bcr.git -b main vendor/bcr
 
+rm -rf hardware/google/pixel && git clone https://github.com/LineageOS/android_hardware_google_pixel -b lineage-21.0 hardware/google/pixel
+
+rm -rf hardware/google/graphics/common && git clone https://github.com/LineageOS/android_hardware_google_graphics_common -b lineage-21.0 hardware/google/graphics/common
+
+rm -rf hardware/google/pixel-sepolicy && git clone https://github.com/LineageOS/android_hardware_google_pixel-sepolicy -b lineage-21.0 hardware/google/pixel-sepolicy
+
+rm -rf hardware/google/gchips && git clone https://github.com/LineageOS/android_hardware_google_gchips -b lineage-21.0 hardware/google/gchips
+
+rm -rf system/core && git clone https://github.com/kenway214/derp_system_core.git -b 14-pixel system/core
+
+rm -rf device/derp/sepolicy && git clone https://github.com/kenway214/device_derp_sepolicy.git -b 14-pixel device/derp/sepolicy
+
+rm -rf vendor/google/pixel && git clone https://gitlab.com/pixel_cheetah/derp_vendor_google_pixel.git -b 14-pixel vendor/google/pixel
+
 rm -rf device/google/pantah/vendorsetup.sh
