87ace0a792cd8805ae76750bb18e6d8b1cf6c86d
kenway214 <kenway214@outlook.com>
2023-12-23 16:46:57 +0530

    cheetah: matrixx-14-ify

--- lineage_cheetah.mk
+++ lineage_cheetah.mk
@@ -14,6 +14,12 @@ $(call inherit-product, device/google/gs201/lineage_common.mk)
 
 include device/google/pantah/cheetah/device-lineage.mk
 
+# Call the BCR setup
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
+# PixelParts
+include packages/apps/PixelParts/device.mk
+
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7 Pro
@@ -22,6 +28,31 @@ PRODUCT_NAME := lineage_cheetah
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
+
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# GAPPS
+WITH_GMS := true
+WITH_GAPPS := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := true
+
+#Matrixx Maintainer Info
+MATRIXX_MAINTAINER := Kenway
+MATRIXX_CHIPSET := Tensor G2
+MATRIXX_BATTERY := 5000mAh
+MATRIXX_DISPLAY := 3120x1440
+EXTRA_UDFPS_ANIMATIONS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=cheetah \
---
+++ vendorsetup.sh
@@ -0,0 +1,26 @@
+echo 'Starting to clone stuffs needed to build for cheetah'
+
+# Device common
+echo 'Cloning common device tree'
+
+git clone https://github.com/kenway214/device_google_gs201.git -b lineage-21 device/google/gs201
+git clone https://github.com/kenway214/device_google_gs101.git -b lineage-21 device/google/gs101
+git clone https://github.com/kenway214/device_google_gs-common.git -b lineage-21 device/google/gs-common
+
+# Kernel
+echo 'Cloning kernel tree'
+git clone --depth=1 --recursive https://github.com/kenway214/kernel_google_gs201.git -b lineage-21 kernel/google/gs201/private/gs-google
+
+# Vendor
+echo 'Cloning vendor tree'
+git clone https://github.com/TheMuppets/proprietary_vendor_google_cheetah.git -b lineage-21 vendor/google/cheetah
+
+# pixel_parts
+echo 'Cloning hardware xiaomi'
+git clone https://github.com/kenway214/packages_apps_PixelParts.git -b lineage-21 packages/apps/PixelParts
+
+# BCR
+echo 'Cloning bcr'
+git clone --depth=1 https://github.com/kenway214/vendor_bcr.git -b main vendor/bcr
+
+rm -rf device/google/pantah/vendorsetup.sh
