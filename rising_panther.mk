91817be459c0429a6201a8edb9afa22f31cd1b48
kenway214 <kenway214@outlook.com>
2023-12-25 13:04:49 +0530

    panther: rising-ify
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- rising_panther.mk
+++ rising_panther.mk
@@ -4,9 +4,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit risingOS vendor configs
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rising/config/rising.mk)
+
+# Call the BCR setup
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
+# UDFPS animations
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
@@ -16,8 +25,29 @@ include device/google/pantah/panther/device-lineage.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
+PRODUCT_DEVICE := panther
+PRODUCT_MANUFACTURER := Google
 PRODUCT_MODEL := Pixel 7
-PRODUCT_NAME := lineage_panther
+PRODUCT_NAME := rising_panther
+
+# RisingOS variables
+RISING_MAINTAINER := Phoenix241
+RISING_CHIPSET := GS201
+
+# Gms variables
+WITH_GMS := true
+
+# Set Pixel launcher as default launcher
+TARGET_PREBUILT_PIXEL_LAUNCHER := true
+
+# Ship Pixel features (adaptivecharging, dreamliner etc)
+TARGET_ENABLE_PIXEL_FEATURES := true
+
+# Use Google telephony framework
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+# Ship Google Camera 
+TARGET_PREBUILT_GOOGLE_CAMERA := true
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
