31b551d04492d05199254ff3acb1d370f6892d0a
kenway <102547769+kenway214@users.noreply.github.com>
2024-02-17 00:27:54 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,20 +21,6 @@ git clone https://github.com/Evolution-X-Devices/packages_apps_PixelParts.git -b
 
 # BCR
 echo 'Cloning bcr'
-git clone --depth=1 https://github.com/kenway214/vendor_bcr.git -b main vendor/bcr
-
-rm -rf hardware/google/pixel && git clone https://github.com/LineageOS/android_hardware_google_pixel -b lineage-21.0 hardware/google/pixel
-
-rm -rf hardware/google/graphics/common && git clone https://github.com/LineageOS/android_hardware_google_graphics_common -b lineage-21.0 hardware/google/graphics/common
-
-rm -rf hardware/google/pixel-sepolicy && git clone https://github.com/LineageOS/android_hardware_google_pixel-sepolicy -b lineage-21.0 hardware/google/pixel-sepolicy
-
-rm -rf hardware/google/gchips && git clone https://github.com/LineageOS/android_hardware_google_gchips -b lineage-21.0 hardware/google/gchips
-
-rm -rf system/core && git clone https://github.com/kenway214/derp_system_core.git -b 14-pixel system/core
-
-rm -rf device/derp/sepolicy && git clone https://github.com/kenway214/device_derp_sepolicy.git -b 14-pixel device/derp/sepolicy
-
-rm -rf vendor/gms && git clone https://gitlab.com/pixel_cheetah/vendor_gms_derp.git -b 14 vendor/gms
+git clone --depth=1 https://github.com/Evolution-X/vendor_bcr.git -b udc vendor/bcr
 
 rm -rf device/google/pantah/vendorsetup.sh
